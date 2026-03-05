790b39e7fc2b6b2d0c03f7a8a00dc1d6586dbba6
Leandrogtgs <131177821+Leandrogtgs@users.noreply.github.com>
2026-03-05 03:28:14 -0300

    Delete minha_pagina directory

--- minha_pagina/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Contato(models.Model):
-    nome = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=20)
-
-    def __str__(self):
-        return f"{self.nome} - {self.telefone}"
